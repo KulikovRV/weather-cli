6f3f8e5fe1c42452bf367141b269e1b1e5aee53a
Roman Kulikov <rkulikov2013@gmail.com>
2022-08-18 11:57:34 +0300

    add: save token in file for user

--- weather.js
+++ weather.js
@@ -1,7 +1,16 @@
 #!/usr/bin/env node
 import getArgs from './helpers/args.js';
-import { printHelp } from './services/log.service.js';
-import saveKeyValue from './services/storage.service.js';
+import { printHelp, printSuccess, printError } from './services/log.service.js';
+import { saveKeyValue, getKeyValue } from './services/storage.service.js';
+
+const saveToken = async (token) => {
+  try {
+    await saveKeyValue('token', token);
+    printSuccess('Токен Сохранен');
+  } catch (error) {
+    printError(error.message);
+  }
+};
 
 const initCli = () => {
   const args = getArgs(process.argv);
@@ -11,7 +20,7 @@ const initCli = () => {
   }
 
   if (args.t) {
-    saveKeyValue('token', args.t);
+    return saveToken(args.t);
   }
 };
 
